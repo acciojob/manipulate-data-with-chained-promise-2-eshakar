bc007251486af997e127dce4c30c73c350ff4c10
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-23 18:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+function manipulateArrayWithPromises() {
+	const outputDiv = document.getElementById('output');
+
+	return new Promise(resolve => {
+		const initialArray = [1, 2, 3, 4];
+		setTimeout(() => {
+			resolve(initialArray);
+		}, 3000);
+	})
+	.then(array => {
+		return new Promise(resolve => {
+			const filteredArray = array.filter(number => number % 2 === 0);
+
+			setTimeout(() => {
+				outputDiv.textContent = filteredArray;
+				resolve(filteredArray);
+			}, 1000);
+		});
+	})
+	.then(filteredArray => {
+		return new Promise(resolve => {
+			const multipliedArray = filteredArray.map(number => number * 2);
+
+			setTimeout(() => {
+				outputDiv.textContent = multipliedArray;
+				resolve(multipliedArray);
+			}, 2000);
+		});
+	})
+	.catch(error => {
+		console.error('Error in promise chain: ', error);
+	});
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	manipulateArrayWithPromises();
+});
+
+
